9080b98e300d643aaa033027138a1676ac456828
kritika <kritikapandey81@gmail.com>
2017-07-12 13:01:43 +0530

    done changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
-def abc
+def sleepandfood
 
 
+end
+
+def relationship
+end
+def parenting
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
 root 'welcome#index'
-get 'welcome/abc'
+get 'welcome/sleepandfood', to: 'welcome#sleepandfood'
+get 'welcome/relationship',to: 'welcome#relationship'
+get 'welcome/parenting', to: 'welcome#parenting'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
